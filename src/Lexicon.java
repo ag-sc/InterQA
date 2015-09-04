144225f528f720a2edc71f71a2e3b57d8bb16a9a
Philipp Cimiano <cimiano@ip-129-70-146-89.wlan.dyn.cit-ec.net>
2015-09-04 10:59:22 +0200

    commiting newly added elements

---
+++ src/Lexicon.java
@@ -0,0 +1,32 @@
+import java.util.HashMap;
+import java.util.List;
+import de.citec.sc.matoll.core.LexicalEntry;
+
+
+public class Lexicon {
+
+	HashMap<String,HashMap<String,List<LexicalEntry>>> lexicon_index;
+	
+	
+	public Lexicon(de.citec.sc.matoll.core.Lexicon lexicon)
+	{
+		lexicon_index = new HashMap<String,HashMap<String,List<LexicalEntry>>>();
+		
+		// create indices
+		
+	}
+	
+	public List<LexicalEntry> getEntriesbyType(String type)
+	{
+		// return all entries by type
+		return null;
+	}
+	
+	public List<String> getLemmasByType(String type)
+	{
+		// return all canonical Forms for a given type
+		return null;
+	}
+	
+	
+}
---
+++ src/VerbElement.java
@@ -0,0 +1,57 @@
+import java.util.List;
+
+import de.citec.sc.matoll.core.LexicalEntry;
+
+public class VerbElement implements ParsableElement {
+
+	Lexicon lexicon;
+	
+	List<LexicalEntry> entries;
+	
+	String type;
+	
+	String argument; // essentially subject, object etc.
+	
+	public VerbElement(Lexicon lexicon, String type)
+	{
+		this.lexicon = lexicon;
+		this.type = type;
+	}
+	
+	@Override
+	public String parse(String string) {
+		// get all verbs from lexicon
+		// extend lexicon by a method that return all lexical entries of a particular type
+		// add all possible lexical entries to the list above
+		
+		String rest = null;
+		
+		List<String> lemmas = lexicon.getLemmasByType(type);
+		
+		for (String lemma: lemmas)
+		{
+			if (true)
+			{
+				return rest;
+			}
+		}
+		
+		return string;
+
+	}
+
+	@Override
+	public List<String> lookahead(List<String> selections) {
+		
+		return lexicon.getLemmasByType("http://www.lexinfo.net/ontology/2.0/lexinfo#TransitiveFrame");
+	}
+
+	@Override
+	public List<String> getInstances() {
+		// do something really crazy
+		// dobj 
+		// SPARQL repository
+		return null;
+	}
+
+}
