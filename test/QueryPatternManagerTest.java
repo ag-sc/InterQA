4f60119dcf4ead5a82b97758ba6a45d05e944e8c
Mariano Rico <mariano.rico@upm.es>
2015-11-02 17:55:28 +0100

    detected new error

--- test/QueryPatternManagerTest.java
+++ test/QueryPatternManagerTest.java
@@ -16,7 +16,7 @@ import java.util.List;
 public class QueryPatternManagerTest {
 
     @Test
-    public void testSomething() {
+    public void testDeleteSecond() {
         // Load lexicon
         Lexicon lexicon = new Lexicon();
         lexicon.load("resources/dbpedia_en.rdf");
@@ -52,6 +52,68 @@ public class QueryPatternManagerTest {
         //UI deletes last selection again (deletes everything)
         qm.userSentence("");
         opts = qm.getUIoptions();  // you should get "all" again
+        Assert.assertTrue(opts.get(0).equals("give me"));
+
+    }
+
+    @Test
+    public void testDeleteFirst() {
+        // Load lexicon
+        Lexicon lexicon = new Lexicon();
+        lexicon.load("resources/dbpedia_en.rdf");
+        InstanceSource instances = new InstanceSource("http://dbpedia.org/sparql");
+
+        // Load query patterns
+        QueryPatternManager qm = new QueryPatternManager();
+        QueryPattern1_1 qp11 = new QueryPattern1_1(lexicon);
+        QueryPattern9_1 qp21 = new QueryPattern9_1(lexicon, instances);
+        QueryPattern9_2 qp22 = new QueryPattern9_2(lexicon, instances);
+        qm.addQueryPattern(qp11);
+        qm.addQueryPattern(qp21);
+        qm.addQueryPattern(qp22);
+
+        List<String> opts = qm.getUIoptions();  // "what", "give me", "who"
+        qm.userSentence("give me"); //Sends the user selection to the qm
+        opts = qm.getUIoptions();  //get options available for last selection: many of them
+
+        qm.userSentence("");  //Equivalent to remove "give me"
+        opts = qm.getUIoptions();  // you should get "what", "give me", "who" again
+
+        Assert.assertTrue(opts.get(0).equals("what"));
+
+    }
+
+    @Test
+    public void testStartsEmpty() {
+        // Load lexicon
+        Lexicon lexicon = new Lexicon();
+        lexicon.load("resources/dbpedia_en.rdf");
+        InstanceSource instances = new InstanceSource("http://dbpedia.org/sparql");
+
+        // Load query patterns
+        QueryPatternManager qm = new QueryPatternManager();
+        QueryPattern1_1 qp11 = new QueryPattern1_1(lexicon);
+        QueryPattern9_1 qp21 = new QueryPattern9_1(lexicon, instances);
+        QueryPattern9_2 qp22 = new QueryPattern9_2(lexicon, instances);
+        qm.addQueryPattern(qp11);
+        qm.addQueryPattern(qp21);
+        qm.addQueryPattern(qp22);
+
+        StringBuffer parsedText = new StringBuffer();
+        List<String> opts = qm.getUIoptions();  // "what", "give me", "who"
+
+        qm.userSentence(""); //Sends the user selection to the qm
+        opts = qm.getUIoptions();  //get options available for last selection: "all"
+
+        qm.userSentence("give me"); //Sends the user selection to the qm
+        opts = qm.getUIoptions();  //get options available for last selection: many of them
+
+        qm.userSentence("give meall");
+        opts = qm.getUIoptions();  // you should get "all" again
+
+        qm.userSentence("give me"); //Sends the user selection to the qm
+        opts = qm.getUIoptions();  //get options available for last selection: many of them
+
         Assert.assertTrue(opts.get(0).equals("all"));
 
     }
