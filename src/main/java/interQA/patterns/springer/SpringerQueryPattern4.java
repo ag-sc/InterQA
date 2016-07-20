f17a377317e958de56d6a70e706a319dbe39074b
Mert Ince <mert_i@hotmail.com>
2016-07-20 15:08:23 +0200

    Springer Pattern 4 is fixed

--- src/main/java/interQA/patterns/springer/SpringerQueryPattern4.java
+++ src/main/java/interQA/patterns/springer/SpringerQueryPattern4.java
@@ -74,7 +74,7 @@ public class SpringerQueryPattern4 extends QueryPattern{
 		
 		Set<String> SPARQLQueries = new HashSet<>();
 
-		SparqlQueryBuilder sqb = new SparqlQueryBuilder();
+		
 		
 		PropertyElement prop_element = (PropertyElement) elements.get(1);
 		InstanceElement name_literal = (InstanceElement) elements.get(2);
