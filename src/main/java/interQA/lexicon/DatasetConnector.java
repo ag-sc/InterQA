7d422167e132eed0914077144e70df17227db3dd
Mariano Rico <mariano.rico@upm.es>
2016-08-05 15:12:50 +0200

    weird

--- src/main/java/interQA/lexicon/DatasetConnector.java
+++ src/main/java/interQA/lexicon/DatasetConnector.java
@@ -56,6 +56,7 @@ public class DatasetConnector {
     }
     public void saveCacheToDisk(){
         cacheAsk.saveCacheToDisk();
+        cacheSel.saveCacheToDisk();
     }
 
     public String getEndpoint(){
