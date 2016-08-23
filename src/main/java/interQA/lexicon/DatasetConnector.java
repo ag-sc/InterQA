620507523a8a6c69417741fb35fccfd3eeb534e3
Mert Ince <mert_i@hotmail.com>
2016-08-23 17:33:58 +0200

    Springer Specific Instance

--- src/main/java/interQA/lexicon/DatasetConnector.java
+++ src/main/java/interQA/lexicon/DatasetConnector.java
@@ -39,7 +39,7 @@ public class DatasetConnector {
     Vocabulary vocab;
     List<String> labelProperties;
     Language lang;
-
+    String gYearProperty;
 
     public DatasetConnector(String url, Language language, USECASE usecase) {
 
@@ -52,6 +52,11 @@ public class DatasetConnector {
                 
             default: {
                 labelProperties.add("http://www.w3.org/2000/01/rdf-schema#label");
+            }
+            case SPRINGER:{
+                labelProperties.add("http://lod.springer.com/data/ontology/property/confName");
+                labelProperties.add("http://lod.springer.com/data/ontology/property/confAcronym");
+                gYearProperty = "http://lod.springer.com/data/ontology/property/confYear";
             }
         }
     }
@@ -110,6 +115,7 @@ public class DatasetConnector {
     public void fillInstances(Element element, QueryBuilder builder, String i_var) {
                         
         String label_var = "l";
+        String gYear_var = "y";
         
         for (IncrementalQuery iq : builder.getQueries()) {
               
@@ -126,7 +132,7 @@ public class DatasetConnector {
                 QuerySolution result   = results.nextSolution();
                 RDFNode       instance = result.get(i_var);
                 RDFNode       label    = result.get(label_var);
-                                                
+                RDFNode       gYear    = result.get(gYear_var);                                  
                 if (instance != null) {
                     
                     LexicalEntry entry = new LexicalEntry();
@@ -138,8 +144,17 @@ public class DatasetConnector {
                         if (label != null && label.isLiteral() &&
                            (label.asLiteral().getLanguage() == null ||
                             label.asLiteral().getLanguage().equals(lang.toString().toLowerCase()))) {
-                                                        
-                            entry.setCanonicalForm(label.asLiteral().getString());
+                               
+                                                    
+                            String year;
+                            if(gYear!=null){
+                                year = gYear.asLiteral().getLexicalForm().substring(0,4);
+                                entry.setCanonicalForm(label.asLiteral().getString()+" "+year);
+                            }
+                            else{
+                              entry.setCanonicalForm(label.asLiteral().getString());   
+                            }
+                            
                             element.addToIndex(label.asLiteral().getString(),entry);
                         }
                     }
@@ -165,16 +180,18 @@ public class DatasetConnector {
             }
         }  
         
-        // TODO if SPRINGER, then additonally get confName/Acronym + confYear
+        
 
     }
     
-    private ElementOptional label(String i_var, String label_var) {
+    private ElementGroup label(String i_var, String label_var) {
+        
+        ElementGroup eg = new ElementGroup();
         
         if (labelProperties.size() == 1) {
             ElementGroup u = new ElementGroup();
             u.addTriplePattern(new Triple(toVar(i_var),toResource(labelProperties.get(0)),toVar(label_var)));
-            return new ElementOptional(u);
+            eg.addElement(new ElementOptional(u));
         }
         else {
             ElementUnion union = new ElementUnion();
@@ -183,8 +200,16 @@ public class DatasetConnector {
                  u.addTriplePattern(new Triple(toVar(i_var),toResource(p),toVar(label_var)));
                  union.addElement(u);
             }
-            return new ElementOptional(union);
+            
+            eg.addElement(new ElementOptional(union));
         }
+        if(gYearProperty!=""){
+                ElementGroup u = new ElementGroup();
+                u.addTriplePattern(new Triple(toVar(i_var),toResource(gYearProperty),toVar("y")));
+                eg.addElement(new ElementOptional(u));
+            }
+        
+        return eg;
     }
 
     
